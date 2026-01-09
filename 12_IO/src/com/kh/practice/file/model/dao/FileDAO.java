8608a694fd0e558a995cc33964d1f185a449886d
user1 <user1@DESKTOP-QGKHJAO>
2026-01-09 11:34:49 +0900

    13일차 수업내용 중간 커밋

--- 12_IO/src/com/kh/practice/file/model/dao/FileDAO.java
+++ 12_IO/src/com/kh/practice/file/model/dao/FileDAO.java
@@ -1,6 +1,10 @@
 package com.kh.practice.file.model.dao;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class FileDAO {
 	public boolean checkName(String file) {
@@ -11,16 +15,65 @@ public class FileDAO {
 	}
 	
 	public void fileSave(String file, String s) {
+		FileWriter f = null;
 		
+		try {
+			f = new FileWriter(file); // 파일이 없다면 생성
+			f.write(s);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				f.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	public StringBuilder fileOpen(String file) {
-		StringBuilder sb = null;
+		StringBuilder sb = new StringBuilder();
+		FileReader f = null;
+		
+		try {
+			f = new FileReader(file);
+			
+			int value = 0;
+			while((value = f.read()) != -1) {
+				sb.append((char)value);
+			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				f.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 		
 		return sb;
 	}
 	
 	public void fileEdit(String file, String s) {
+		FileWriter f = null;
 		
+		try {
+			f = new FileWriter(file, true); // 이어쓰기.
+			f.write(s);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				f.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
--- 12_IO/src/com/kh/practice/file/view/FileMenu.java
+++ 12_IO/src/com/kh/practice/file/view/FileMenu.java
@@ -1,8 +1,5 @@
 package com.kh.practice.file.view;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 import com.kh.practice.file.controller.FileController;
@@ -20,6 +17,7 @@ public class FileMenu {
 			System.out.println("9. 끝내기");
 			System.out.print("메뉴 번호 : ");
 			int menu = sc.nextInt();
+			sc.nextLine();
 
 			switch (menu) {
 			case 1:
@@ -41,34 +39,22 @@ public class FileMenu {
 	}
 
 	public void fileSave() {
-		System.out.println("파일에 저장할 내용을 입력하세요.");
-		System.out.println("ex끝it 이라고 입력하면 종료됩니다");
-		System.out.print("내용 : ");
-
-		StringBuilder sb = new StringBuilder();
-		while (true) {
-			String line = sc.nextLine();
-			if (line.equals("ex끝it")) {
-				break;
-			}
-			sb.append(line+"\n");
-		}
+		StringBuilder sb = addText();
 
 		while (true) {
 			System.out.print("저장할 파일 명을 입력해주세요(ex. myFile.txt) : ");
 			String file = sc.nextLine();
 
-			if (fc.checkName(file)) { // 이미 존재함 true
-				System.out.println("이미 존재하는 파일입니다. 덮어쓰시겠습니까?(y/n)");
-				char ch = sc.nextLine().toUpperCase().charAt(0);
+			if(!fc.checkName(file)) { // 중복이 없으면
+				fc.fileSave(file, sb); // 바로 저장
+				break;
+			}
+			
+			// 중복이면
+			System.out.print("이미 존재하는 파일입니다. 덮어쓰시겠습니까?(y/n)");
+			char ch = sc.nextLine().toUpperCase().charAt(0);
 
-				if (ch == 'Y') {
-					fc.fileSave(file, sb);
-					break;
-				}else {
-					continue;
-				}
-			}else {
+			if (ch == 'Y') {
 				fc.fileSave(file, sb);
 				break;
 			}
@@ -77,43 +63,45 @@ public class FileMenu {
 
 	public void fileOpen() {
 		System.out.print("열 파일 명 : ");
-		String file = sc.next();
+		String file = sc.nextLine();
 		
-		if (fc.checkName(file)) { // 이미 존재함 true
-			System.out.println(fc.fileOpen(file));
-		}else {
+		if (!fc.checkName(file)) { // 존재하지 않으면
 			System.out.println("없는 파일입니다.");
+			return;
+			// throw new RuntimeException("존재하지 않는 파일입니다.");
 		}
+		
+		System.out.println(fc.fileOpen(file));
 	}
 
 	public void fileEdit() {
 		System.out.print("수정할 파일 명 : ");
-		String file = sc.next();
+		String file = sc.nextLine();
 		
-		if (fc.checkName(file)) { // 이미 존재함 true
-			System.out.println("이미 존재하는 파일입니다. 덮어쓰시겠습니까?(y/n)");
-			char ch = sc.nextLine().toUpperCase().charAt(0);
-
-			if (ch == 'Y') {
-				System.out.println("파일에 저장할 내용을 입력하세요.");
-				System.out.println("ex끝it 이라고 입력하면 종료됩니다");
-				System.out.print("내용 : ");
-
-				StringBuilder sb = new StringBuilder();
-				while (true) {
-					String line = sc.nextLine();
-					if (line.equals("ex끝it")) {
-						break;
-					}
-					sb.append(line+"\n");
-				}
-				
-				fc.fileSave(file, sb);
-			}
-			
-			System.out.println(fc.fileOpen(file));
-		}else {
+		if (!fc.checkName(file)) { // 존재하지 않으면
 			System.out.println("없는 파일입니다.");
+			return;
 		}
+		
+		StringBuilder sb = addText();
+		fc.fileEdit(file, sb);
+	}
+	
+	private StringBuilder addText() {
+		StringBuilder sb = new StringBuilder();
+
+		while (true) {
+			System.out.println("파일에 저장할 내용을 입력하세요.");
+			System.out.println("ex끝it 이라고 입력하면 종료됩니다");
+			System.out.print("내용 : ");
+
+			String line = sc.nextLine();
+			if (line.equals("ex끝it")) {
+				break;
+			}
+			sb.append(line + "\n");
+		}
+		
+		return sb;
 	}
 }
